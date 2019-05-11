dd5983f0616775a635fe0a5d3e5eaebbfdbe74bd
Mc Donald <fuentebellamcdonald@gmail.com>
2019-05-11 00:09:42 -0700

    Changes by Sir Ryan Dell

--- application/modules/admin/views/dashboard/dashboard.php
+++ application/modules/admin/views/dashboard/dashboard.php
@@ -17,7 +17,8 @@
         <!-- /.col -->
         <div class="col-md-3 col-sm-6 col-xs-12">
           <div class="info-box">
-            <span class="info-box-icon bg-red"><i><?php echo $this->setting->currency_symbol?></i></span>
+            <span class="info-box-icon bg-red"><i class="fa fa-money"></i></span>
+            
 
             <div class="info-box-content">
               <span class="info-box-text"><?php echo lang('today_revenue')?></span>
